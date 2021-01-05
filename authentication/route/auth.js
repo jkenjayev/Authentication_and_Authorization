e90de9a5e41338653f462ae64f1fda9df05ff38d
jkenjayev <javohirwebdev@gmail.com>
2021-01-05 12:19:10 +0500

    added lodash npm package

--- authentication/route/auth.js
+++ authentication/route/auth.js
@@ -2,6 +2,7 @@ const { User, validate } = require("../module/auth");
 const express = require("express");
 const mongoose = require("mongoose");
 const router = express.Router();
+const _ = require("lodash");
 
 router.post("/", async (req, res) => {
   // validate first
@@ -9,13 +10,10 @@ router.post("/", async (req, res) => {
   if (error) return res.status(400).send(error.details[0].message);
 
   let user = await User.findOne({ email: req.body.email });
-  user = new User({
-    name: req.body.name,
-    email: req.body.email,
-    password: req.body.password,
-  });
-
+  user = new User(_.pick(req.body, ["name", "email", "password"]));
   const result = await user.save();
+  
+  res.status(201).send(_.pick(user, ["_id", "name", "email"]));
 });
 
-module.exports = router;
+module.exports = router;  
