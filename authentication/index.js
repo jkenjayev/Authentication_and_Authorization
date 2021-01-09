7fec5d4411a05b465a8775f1a71209a86c6bbda7
jkenjayev <69050649+jkenjayev@users.noreply.github.com>
2021-01-09 17:09:48 +0500

    fixed the bug using the json()

--- authentication/index.js
+++ authentication/index.js
@@ -3,6 +3,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const app = express();
 
+app.use(express.json());
 app.use("/api/user", authRouter);
 
 mongoose.connect("mongodb://localhost/auth",{
